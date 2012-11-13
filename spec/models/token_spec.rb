a0dfe88eb0b08a303bdd5974f1a8b90a7459bbd1
Kyle Lacy <kylewlacy@me.com>
2012-11-13 06:21:20 -0800

    add spec for token model

---
+++ spec/models/token_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Token do
+  it "can belong to a user" do
+    token = Token.create
+    user = User.create
+    user.tokens << token
+
+    user.tokens.should == [token]
+  end
+end
