e9e4837d0fa36529bb74e86ab553d24eda4b9ccb
Kyle Lacy <kylewlacy@me.com>
2012-11-18 18:21:47 -0800

    add type column to user table

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -6,6 +6,8 @@ class CreateUsers < ActiveRecord::Migration
       t.string :email
       t.string :password_hash
 
+      t.string :type
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 2) do
     t.string   "last_name"
     t.string   "email"
     t.string   "password_hash"
+    t.string   "type"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
   end
