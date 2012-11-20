f42ea3870bc53193fca7a5653aba01e5d0f901ff
Kyle Lacy <kylewlacy@me.com>
2012-11-19 20:02:35 -0800

    add student#enrolled_courses

--- models/student.rb
+++ models/student.rb
@@ -1,3 +1,11 @@
 class Student < User
+  has_many :student_courses
 
+  def enrolled_courses
+    student_courses = self.student_courses.map do |student_course|
+      student_course.course
+    end
+
+    (self.courses + student_courses).uniq
+  end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -10,4 +10,24 @@ describe Student do
 
     student.type.should == 'Student'
   end
+
+  it "lists all the courses enrolled" do
+    student = Student.create_account!(
+      :name => 'John Doe',
+      :email => 'john.doe@example.com',
+      :password => 'stillastudent'
+    )
+
+    teacher = Teacher.create_account!(
+      :name => 'Michael Doe',
+      :email => 'michael.doe@example.com',
+      :password => 'imateacher'
+    )
+
+    math = student.create_course!(:name => 'Math')
+    english = teacher.create_course!(:name => 'English')
+    english.students << student
+
+    student.enrolled_courses.should == [math, english]
+  end
 end
